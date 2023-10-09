a2182940d1b5641576c0c6958f11a3fde307b87d
Haris Ahmad <harizahmad100@gmail.com>
2023-10-09 17:42:19 -0400

    connected to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,18 @@
+import {useEffect, useState} from "react";
+
 const App = () => {
+
+    const [players, setPlayers] = useState([])
+
+    useEffect(() => {
+        fetch('/players')
+            .then(response => response.json())
+            .then(data => setPlayers(data._embedded['players']))
+            .catch(error => console.log(error))
+    }, []);
+
     return(
         <div>
-            
         </div>
     );
 }
