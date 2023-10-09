20eb20512fceff489981a1cc15c01ffc48b6fa4c
Haris Ahmad <harizahmad100@gmail.com>
2023-10-09 19:45:30 -0400

    initial table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,35 @@
 import {useEffect, useState} from "react";
+import MUIDataTable from "mui-datatables";
 
 const App = () => {
 
     const [players, setPlayers] = useState([])
+    const [column, setColumn] = useState([])
 
     useEffect(() => {
         fetch('/players')
             .then(response => response.json())
-            .then(data => setPlayers(data._embedded['players']))
+            .then(data => {
+                setPlayers(data._embedded['players'])
+                return data._embedded['players']
+            })
+            .then(data => {
+                let temp = []
+                Object.keys(data[0]).slice(0, -1).forEach(x => {
+                    temp.push(
+                        {
+                            "name": x,
+                            "label": x.charAt(0).toUpperCase() + x.slice(1),
+                        }
+                    )
+                })
+                setColumn(temp)
+            })
             .catch(error => console.log(error))
     }, []);
 
     return(
-        <div>
-        </div>
+        <MUIDataTable columns={column} data={players} title="FIFA Database"/>
     );
 }
 
